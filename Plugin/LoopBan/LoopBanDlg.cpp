ce535ace95e5370154d3f5ae6a80de1db60f1aff
dog194 <514896768@qq.com>
2021-08-27 02:55:34 +0800

    LoopBan，同步跟进更新/修复，Loopban 2.1.0.5->2.1.0.6
    LoopBan，同步跟进更新/修复，
    Bug修复，
    GBK编码支持
    Loopban 2.1.0.5->2.1.0.6

--- Plugin/LoopBan/LoopBanDlg.cpp
+++ Plugin/LoopBan/LoopBanDlg.cpp
@@ -228,6 +228,8 @@ BOOL CLoopBanDlg::SetItem(int index)
 	{
 		// 判断是否已经在列表中，避免重复添加
 		for (int i = 0; i < m_list.GetItemCount(); i++) {
+			if (i == index)
+				continue;
 			if (portrait != _T("")) {
 				// portrait 不为空 ，默认用portrait
 				if (portrait == m_list.GetItemText(i, COLUMN_INDEX_PORTRAIT)) {
--- Plugin/LoopBan/LoopBanInputDlg.cpp
+++ Plugin/LoopBan/LoopBanInputDlg.cpp
@@ -85,7 +85,7 @@ void CLoopBanInputDlg::OnOK()
 // 输入Url -> portrait
 void CLoopBanInputDlg::OnEnKillfocusEditPortrait()
 {
-	CString tmp, tmpP, tmpU;
+	CString tmp, tmpP, tmpU_ori, tmpU;
 	m_edit_portrait.GetWindowText(tmp);
 	if (tmp.GetLength() > 36) {
 		tmpP = GetStringBetween(tmp, _T("id="), _T("&"));
@@ -95,9 +95,13 @@ void CLoopBanInputDlg::OnEnKillfocusEditPortrait()
 		if (tmpP.GetLength() <= 36 && tmpP.GetLength() > 30) {
 			m_edit_portrait.SetWindowTextW(tmpP);
 		}
-		m_edit_uid.GetWindowText(tmpU);
-		if (tmpU == _T("")) {
-			tmpU = GetStringBetween(tmp, _T("un="), _T("&"));
+		m_edit_uid.GetWindowText(tmpU_ori);
+		if (tmpU_ori == _T("")) {
+			tmpU_ori = GetStringBetween(tmp, _T("un="), _T("&"));
+			tmpU = DncodeURI(tmpU_ori);
+			if (tmpU == _T("")) {
+				tmpU = DncodeURI_GBK(tmpU_ori);
+			}
 			m_edit_uid.SetWindowTextW(DncodeURI(tmpU));
 		}
 	}
