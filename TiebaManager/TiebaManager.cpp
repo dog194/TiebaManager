d0e49cb96f0efb178730bbfd3797e8645b0bbeb9
dog194 <514896768@qq.com>
2023-08-24 09:59:35 +0800

    部分第三方库使用延迟加载，库md5校验
    部分第三方库使用延迟加载，库md5校验

--- TiebaManager/TiebaManager.cpp
+++ TiebaManager/TiebaManager.cpp
@@ -22,6 +22,14 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 //
 
 #include "stdafx.h"
+#include <windows.h>
+#include <wincrypt.h>
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <map>
 //#include <vld.h> // Visual Leak Detector，检测内存泄漏的，可以不要
 
 #include "TiebaManager.h"
@@ -64,6 +72,11 @@ CTiebaManagerApp::CTiebaManagerApp()
 // CTiebaManagerApp 初始化
 BOOL CTiebaManagerApp::InitInstance()
 {
+	// 基础库校验
+	if (Check3rdDll() == FALSE) {
+		// 基础库校验失败，直接结束程序
+		exit(0);
+	}
 	// 如果一个运行在 Windows XP 上的应用程序清单指定要
 	// 使用 ComCtl32.dll 版本 6 或更高版本来启用可视化方式，
 	//则需要 InitCommonControlsEx()。  否则，将无法创建窗口。
@@ -189,4 +202,105 @@ int CTiebaManagerApp::ExitInstance()
 	TRACE(_T("退出程序\n"));
 
 	return CWinApp::ExitInstance();
+}
+
+BOOL CTiebaManagerApp::Check3rdDll() {
+	CString cPath, info;
+	cPath.ReleaseBuffer(GetModuleFileName(NULL, cPath.GetBuffer(MAX_PATH), MAX_PATH));
+	if (cPath.Replace(_T("TiebaManager.exe"), _T("")) != 1) {
+		AfxMessageBox(_T("本体文件名异常 TiebaManager.exe"), MB_ICONERROR);
+		return FALSE;
+	}
+	std::map<CString, std::tuple<std::string, CString>> dllList;
+	dllList[L"7za.exe"] =			std::make_tuple("43141e85e7c36e31b52b22ab94d5e574", _T("19.0.0"));
+	dllList[L"abseil_dll.dll"] =	std::make_tuple("e982db6ff3509ecc4e605952a09e3365", _T("搭配24.0的Protobuf的版本"));
+	dllList[L"curl.exe"] =			std::make_tuple("a29f0441ebcc19dcf73352ce5f0e1f94", _T("7.78.0"));
+	dllList[L"libcurl.dll"] =		std::make_tuple("98618ff785f6fa8d3c2022c5dc2241f3", _T("7.78.0"));
+	dllList[L"libprotobuf.dll"] =	std::make_tuple("04e77a54562e24d4b2631f0579c54d84", _T("24.0.0"));
+	dllList[L"libprotobuf-lite.dll"] =		std::make_tuple("77e7b0be675baa64457bed5a6b63d62b", _T("24.0.0"));
+	dllList[L"opencv_core453.dll"] =		std::make_tuple("1e69b396576dc9eba0291454fbafda97", _T("4.5.3"));
+	dllList[L"opencv_dnn453.dll"] =			std::make_tuple("96c9589e5ce0795d6df128188029fc24", _T("4.5.3"));
+	dllList[L"opencv_imgcodecs453.dll"] =	std::make_tuple("adbd6f9c069f77a05fbf9ecaea424233", _T("4.5.3"));
+	dllList[L"opencv_imgproc453.dll"] =		std::make_tuple("6674e91107254ac1898ee05da4510b3e", _T("4.5.3"));
+	dllList[L"opencv_wechat_qrcode453.dll"] = std::make_tuple("858f0493df447f6adbbd7644cc267ac0", _T("4.5.3"));
+	dllList[L"sqlite3.dll"] =	std::make_tuple("084fb64052c55679d78f122b9e5d2e1b", _T("3.41.2"));
+	dllList[L"tinyxml2.dll"] =	std::make_tuple("b7a0648e9a40e9599a7ed2e4ee904c40", _T("9.0.0"));
+	info = _T("");
+	for (const auto& dll : dllList) {
+		std::string dll_md5 = GetFileMd5(cPath, dll.first);
+		CString c_dll_md5(dll_md5.c_str());
+		CString c_md5_record(std::get<0>(dll.second).c_str());
+		if (dll_md5 != std::get<0>(dll.second)) {
+			info += _T("文件名：") + dll.first + _T(" 缺少 - ") + c_dll_md5 + _T("\n");
+			info += _T("所需版本：") + std::get<1>(dll.second) + _T(" - ") + c_md5_record + _T("\n");
+		}
+	}
+	if (info != _T("")) {
+		info += _T("\n请到群里单独下载基础库包解压到根目录 ※覆盖更新※");
+		AfxMessageBox(info, MB_ICONERROR);
+		return FALSE;
+	}
+	return TRUE;
+}
+
+std::string CTiebaManagerApp::GetFileMd5(const CString& fPath, const CString& fName) {
+	std::ifstream file(fPath + fName, std::ios::binary);
+	if (!file) {
+		return "NO FOUND";
+	}
+
+	// Acquire a cryptographic provider context handle
+	HCRYPTPROV hProv = 0;
+	if (!CryptAcquireContext(&hProv, NULL, NULL, PROV_RSA_FULL, CRYPT_VERIFYCONTEXT)) {
+		// "CryptAcquireContext failed"
+		return "ERROR";
+	}
+
+	// Create an empty hash object
+	HCRYPTHASH hHash = 0;
+	if (!CryptCreateHash(hProv, CALG_MD5, 0, 0, &hHash)) {
+		// "CryptCreateHash failed"
+		CryptReleaseContext(hProv, 0);
+		return "ERROR";
+	}
+
+	// Hash the data
+	char buf[1024];
+	while (file.read(buf, sizeof(buf))) {
+		if (!CryptHashData(hHash, reinterpret_cast<BYTE*>(buf), sizeof(buf), 0)) {
+			// "CryptHashData failed"
+			CryptDestroyHash(hHash);
+			CryptReleaseContext(hProv, 0);
+			return "ERROR";
+		}
+	}
+	if (!CryptHashData(hHash, reinterpret_cast<BYTE*>(buf), static_cast<DWORD>(file.gcount()), 0)) {
+		// "CryptHashData failed"
+		CryptDestroyHash(hHash);
+		CryptReleaseContext(hProv, 0);
+		return "ERROR";
+	}
+
+	// Get the hash value
+	DWORD cbHash = 16;
+	BYTE rgbHash[16];
+	if (!CryptGetHashParam(hHash, HP_HASHVAL, rgbHash, &cbHash, 0)) {
+		// "CryptGetHashParam failed"
+		CryptDestroyHash(hHash);
+		CryptReleaseContext(hProv, 0);
+		return "ERROR";
+	}
+
+	std::ostringstream oss;
+	// Print the result
+	for (int i = 0; i < cbHash; i++) {
+		oss << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(rgbHash[i]);
+	}
+	std::string md5Str = oss.str();
+
+	// Clean up
+	CryptDestroyHash(hHash);
+	CryptReleaseContext(hProv, 0);
+
+	return md5Str;
 }
